22ca88371e38db493d8aa24f1a8e1b7721725c78
Cupreous <michaelrusso242@aol.com>
2023-10-31 16:50:19 -0400

    Initial setup: Adds the form submit that takes any number Includes the initial prop setup Includes AccountBalance

--- bank-of-react/src/components/Credits.js
+++ bank-of-react/src/components/Credits.js
@@ -5,11 +5,29 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+  const { credits, currentBalance } = props;
+
+  const addCredit = (e) =>
+  {
+
+  }
   return (
     <div>
       <h1>Credits</h1>
+      
+      
+
+      <form onSubmit={addCredit}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" step = "any"/>
+        <button type="submit">Add Credit</button>
+      </form>
+      <br/>
+        <AccountBalance accountBalance={currentBalance}/>
+      <br/>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
